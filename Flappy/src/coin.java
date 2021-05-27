1b215c56c9bef26969558bf3145d2aaff7e52476
nkdus <nkdus@LAPTOP-0GNFMTFE>
2021-05-27 08:55:53 -0700

    Coin and sort of setting up the setting up score class which is located in frame

--- Flappy/src/coin.java
+++ Flappy/src/coin.java
@@ -1,4 +1,21 @@
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 
 public class coin {
 
+	private int x, y;
+	private int vx, vy;
+	private int width;
+	private int height;
+	
+	private Image img;
+	
+	public coin() {
+	img = getImage("pixil-frame-0(1).png");		   
+	   
 }
+	
+	public coin("pixil-frame-0(1).png") {
+		
+	}
